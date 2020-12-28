cfd3309734ddd173760e4b0f0bdffc4ffc3e432f
Rafael Barrera Oro <rafael@MacBook-Pro-de-.local>
2020-12-28 04:01:11 -0300

    bunker arena

---
+++ js/arena.js
@@ -0,0 +1,200 @@
+// Constants
+
+const WIDTH = 800;
+const HEIGHT = 600;
+const RADIUS = 10;
+const LAPSE = 0.0001;
+const ZERO = new Vector(0, 0);
+const DELTA = 0.0001;
+
+const PLAYERS = {
+	0: new Player(0, 'Vieira'),
+	1: new Player(1, 'El Tío'),
+	2: new Player(2, 'Sergini'),
+	3: new Player(3, 'Grido'),
+	4: new Player(4, 'Rafa'),
+	5: new Player(5, 'Piotr'),
+	6: new Player(6, 'Mausy'),
+	7: new Player(7, 'Arielaki'),
+	8: new Player(8, 'GREgO'),
+	9: new Player(9, 'Dieguito'),
+	10: new Player(10, 'Lucardo'),
+	11: new Player(11, 'Negra'),
+	12: new Player(12, 'Manu'),
+	13: new Player(13, 'Cristo'),
+	14: new Player(14, 'Mauricio'),
+	15: new Player(15, 'Axel'),
+	16: new Player(16, 'Wences'),
+	17: new Player(17, 'Pablito'),
+	18: new Player(18, 'Leander'),
+	19: new Player(19, 'Eddie')
+}
+
+// Variables
+
+let state = 0;
+
+// Classes
+
+function Player(id, name) {
+	this.id = id;
+	this.name = name;
+	this.pos = ZERO;
+	this.color = '#FFFFFF';
+	this.speed = ZERO;
+	this.acceleration = ZERO;
+	this.active = false;
+}
+
+function Vector(x, y) {
+	this.x = x;
+	this.y = y;
+	this.add = function (vector) {
+		return new Vector(this.x + vector.x, this.y + vector.y);
+	};
+	this.sub = function (vector) {
+		return new Vector(this.x - vector.x, this.y - vector.y);
+	};
+	this.mul = function (num) {
+		return new Vector(this.x * num, this.y * num);
+	};
+	this.dot = function (vector) {
+		return this.x * vector.x + this.y * vector.y;
+	};
+	this.abs = function () {
+		return Math.sqrt(this.x * this.x + this.y * this.y);
+	}
+	this.normal = function () {
+		return new Vector(1, -(this.x / this.y));
+	}
+}
+
+// Functions
+
+function drag(event) {
+	event.dataTransfer.setData("text", event.target.id);
+}
+
+function drop(event) {
+	let id = event.dataTransfer.getData('text');
+	let element = document.getElementById(id);
+	let color = getRandomColor();
+	element.style.backgroundColor = color;
+	element.draggable = false;
+	placePlayer(id, getCanvasCoordinates(getCanvas(), event), color);
+	render();
+}
+
+function placePlayer(id, pos, color) {
+	let player = PLAYERS[id];
+	player.pos = pos;
+	player.color = color;
+	player.active = true;
+}
+
+function getRandomColor() {
+	return '#' + Math.floor(Math.random()*16777215).toString(16);
+}
+
+function getCanvas() {
+	return document.getElementById('arenaCanvas');
+}
+
+function getCanvasCoordinates(canvas, event) {
+	const rect = canvas.getBoundingClientRect()
+	const x = event.clientX - rect.left
+	const y = event.clientY - rect.top
+	return new Vector(x, y);
+}
+
+function start() {
+	state = 1;
+	loop();
+}
+
+function loop() {
+	Object.values(PLAYERS)
+		.filter(p => p.active)
+		.map(p => update(p));
+
+	render();
+
+	if (state === 1) {
+		setInterval(loop, 1);
+	}
+}
+
+function update(player) {
+	let closestPlayer = getClosestPlayer(player);
+
+	if (inContact(player, closestPlayer)) {
+		console.info(player.name + ' le pego a ' + closestPlayer.name);
+		let direction = closestPlayer.pos.sub(player.pos);
+		closestPlayer.acceleration = direction;
+		closestPlayer.pos = player.pos.add(direction.mul(((RADIUS * 2) + 1) / direction.abs()));
+	} else {
+		let newPos = player.pos.add(player.speed.mul(LAPSE));
+
+		if (isWithinBoundries(newPos) && !collides(player, newPos)) {
+			player.pos = newPos;
+			player.speed = player.speed.add(player.acceleration.mul(LAPSE));
+		} else {
+			player.speed = ZERO;
+		}
+
+		player.acceleration = closestPlayer.pos.sub(player.pos);
+	}
+}
+
+function inContact(player1, player2) {
+	return player1.pos.sub(player2.pos).abs() <= RADIUS * 2 + DELTA;
+}
+
+function isWithinBoundries(pos) {
+	return pos.x >= RADIUS
+		&& pos.x <= WIDTH - RADIUS
+		&& pos.y >= RADIUS
+		&& pos.y <= HEIGHT - RADIUS;
+}
+
+function collides(player, newPos) {
+	return Object.values(PLAYERS)
+		.filter(p => p !== player)
+		.filter(p => p.pos.sub(newPos).abs() <= RADIUS * 2)
+		.length > 0;
+}
+
+function getClosestPlayer(player) {
+	let closestPlayerId = Object.values(PLAYERS)
+		.filter(p => p !== player)
+		.map(p => {
+			return {
+				'id': p.id,
+				'distance': p.pos.sub(player.pos).abs()
+			}
+		})
+		.sort(function (p1, p2) {
+			return p1['distance'] - p2['distance'];
+		})[0].id;
+
+	return PLAYERS[closestPlayerId];
+}
+
+function render() {
+	let canvas = getCanvas();
+	let ctx = canvas.getContext('2d');
+	clear(ctx);
+	Object.values(PLAYERS).map(player => renderPlayer(ctx, player));
+}
+
+function clear(ctx) {
+	ctx.fillStyle = "white";
+	ctx.fillRect(0, 0, WIDTH, HEIGHT);
+}
+
+function renderPlayer(ctx, player) {
+	ctx.beginPath();
+	ctx.fillStyle = player.color;
+	ctx.arc(player.pos.x, player.pos.y, RADIUS, 0, 2 * Math.PI);
+	ctx.fill();
+}
---
+++ js/vector.js
@@ -0,0 +1,22 @@
+function Vector(x, y) {
+    this.x = x;
+    this.y = y;
+    this.add = function add(vector) {
+        return new Vector(this.x + vector.x, this.y + vector.y);
+    };
+    this.sub = function sub(vector) {
+        return new Vector(this.x - vector.x, this.y - vector.y);
+    };
+    this.mul = function sub(num) {
+        return new Vector(this.x * num, this.y * num);
+    };
+    this.dot = function sub(vector) {
+        return this.x * vector.x + this.y * vector.y;
+    };
+    this.abs = function abs() {
+        return Math.sqrt(this.x * this.x + this.y * this.y);
+    }
+    this.normal = function normal() {
+        return new Vector(1, -(this.x / this.y));
+    }
+}
